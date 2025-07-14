9b793914a940975c10504f80779b970522f5713b
Sephba <mateobate152@gmail.com>
2025-07-13 19:53:59 -0500

    creacion de usuarios

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
-from app.services.user import login_user
+from fastapi import APIRouter, HTTPException
+from typing import Optional
+
+from app.services.user import login_user, register_user
 
 router = APIRouter(prefix="", tags=["Usuarios"])
 
@@ -12,3 +14,19 @@ def login(user_name: str, password: str):
     else:
         print('datos: ', user_name, password)
         return {"status": "ERROR", "detalle": "Credenciales incorrectas"}
+    
+    
+@router.post("/register-query/")
+def register_query(
+    user_name: str,
+    password: str,
+    lat: Optional[float] = None,
+    lon: Optional[float] = None,
+    city_id: Optional[int] = None,
+):
+    user_id = register_user(user_name, password, lat, lon, city_id)
+
+    if user_id:
+        return {"status": "OK", "mensaje": "Usuario creado", "user_id": user_id}
+    else:
+        raise HTTPException(status_code=400, detail="No se pudo crear el usuario")
