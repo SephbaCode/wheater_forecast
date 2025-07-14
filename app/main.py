8a03689eaacb8fc41bd914e23adefbd8afc6319a
Sephba <mateobate152@gmail.com>
2025-07-13 19:27:35 -0500

    agregado creacion y obtencion de acticidades

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
-from app.routers.users import router as user_router   
+from app.routers.users import router as user_router
+from app.routers.router_activity import router as activity_router
 
 from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(title="Planificador Climático")
-app.include_router(user_router, prefix="/usuarios", tags=["Usuarios"])       
-# Middleware CORS (si planeas consumir desde frontend)
+app.include_router(user_router, prefix="/usuarios", tags=["Usuarios"])
+app.include_router(activity_router, prefix="/actividades", tags=["Actividades"])       
 
 # Ruta base
 @app.get("/")
